0c98763dfa6355cc1ab0e4997c3838eceabfcee7
poohharry <poohharry@naver.com>
2022-02-12 20:04:46 +0900

    장바구니 js부분 만드는 중

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,16 @@
 'use strict'
 
+let cart1 = document.getElementsByClassName('basket');
+
 function button1_click() {
 	console.log("버튼1을 누르셨습니다.");
+}
+
+function cart_open() {
+	console.log("버튼1을 누르셨습니다.");
+	cart1.style.backgroundColor = "blue"
+}
+
+function cart_close() {
+
 }
