3358d14703b929d76359028bafa64e19c620575f
poohharry <poohharry@naver.com>
2022-02-16 17:48:12 +0900

    장바구니 삭제 기능완성 및 함수화 완료

--- js/cart.js
+++ js/cart.js
@@ -2,9 +2,9 @@
 
 const main_cart = document.getElementById('basket');
 const inner_cart = document.getElementById('inner_basket');
-let cartNum = 0;
-let cartBox = [];
-let xBox = [];
+let cartNum = 0;      // 클래스 번호
+let cartBox = [];     // 클래스 배열
+let xBox = [];        // 클래스안에 들어간 x버튼 배열(클래스배열의 인덱스와 x버튼 배열의 인덱스가 같음)
 
 /* 
 	들어가야할 요소 및 기능들
@@ -18,12 +18,17 @@ let xBox = [];
 
 // 장바구니 내용을 담고 있는 클래스
 class Cart_content {
-	constructor() {
-		this.name = "";
-		this.img = "";
-		this.content = "";
+	_name;
+	_img;
+	_content;
+	_arrIdx;
+
+	constructor(name, img) {
+		this._name = name;
+		this._img = img;
 	}
-}
+
+}// Cart_content
 
 
 function cart_open() {                     // 장바구니 열기
@@ -37,29 +42,34 @@ function cart_close() {                    // 장바구니 닫기
 }
 
 // 장바구니에 담기
-function in_cart(lo_name, add) {
-	cartBox.push(new Cart_content);
-	cartBox[cartNum].name = lo_name;
-	cartBox[cartNum].img = add;
-	cartBox[cartNum].content = `
-	<div class="inner_cart">
+function in_cart(lo_name, add) {   // (지역이름, 이미지 주소)
+	cartBox.push(new Cart_content(lo_name, add));
+	
+	//클래스에 들어갈 html코드
+	cartBox[cartNum].content = 
+	`<div class="inner_cart">
 	<img src="${add}" class="inner_cart_img">
-	${cartBox[cartNum].name}
-	<button onclick="out_cart(${cartNum})">X</button>
+	${cartBox[cartNum]._name}
+	<button class="Xbutton" onclick="out_cart(${cartNum})">X</button>
 	</div>
-	<hr style="margin: 5px 0;">
-	`;
+	<hr style="margin: 5px 0;">`;
 	inner_cart.innerHTML = all_inner_cart();
+	
 	cartNum++;
-}
+}// in_cart
 
 // 장바구니에서 뺴기
 function out_cart(num) {
+
 	cartBox.splice(num, 1);
 
+	for(let i = 0; i < cartBox.length; i++) {
+		cartBox[i].content = changeContent(i, cartBox[i]._img, cartBox[i]._name);
+	}
+	
 	inner_cart.innerHTML = all_inner_cart();
 	cartNum--;
-}
+}// out_cart
 
 
 // 모든 장바구니 클래스를 합쳐서 리턴
@@ -69,4 +79,14 @@ function all_inner_cart() {
 		inner_contents += cartBox[i].content;
 	}
 	return inner_contents;
+}// all_inner_cart
+
+function changeContent(num, img, name) {
+	let a = `<div class="inner_cart">
+	<img src="${img}" class="inner_cart_img">
+	${name}
+	<button class="Xbutton" onclick="out_cart(${num})">X</button>
+	</div>
+	<hr style="margin: 5px 0;">`;
+	return a;
 }
