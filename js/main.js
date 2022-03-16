312ceb9e62bdee1ecc33f17154de4373fc35c41b
poohharry <poohharry@naver.com>
2022-03-16 16:43:40 +0900

    배경이미지를 모아둘 배열 생성 및, 초기 배경값 설정오류 해결중

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let scrollPosition;
 let bg = $("#bg");
 
+// 스크롤 비율에 따른 배경색 바꾸기
 function getScrollPosition() {
     scrollPosition = Math.floor(($(window).scrollTop() / ($(document).height() - $(window).height())) * 100);
     if(scrollPosition < 33 && scrollPosition >= 0) {
@@ -16,3 +17,5 @@ function getScrollPosition() {
 // 스크롤을 움직일때마다 해당 함수가 실행
 $(window).scroll(getScrollPosition)
 
+// 배경색 배열(아침 -> 밤-> 아침)
+let bgColor = new Array();
