053f6098b379c4d8d7e8d4a3622a0f09e7e56897
poohharry <poohharry@naver.com>
2022-03-21 22:14:26 +0900

    스크롤 비율이 일정 수치를 지나쳤을때 배경이미지 배열의 인덱스에 변화를 주어 스크롤에 따른 배경을 바꾸게 하고 싶음 하드코딩으로 해결은 가능하지만 다른 방법을 생각하는 중

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let scrollPosition;
 let bg = $("#bg");
+let scrollRate = 0;
 
 // 스크롤 비율에 따른 배경색 바꾸기
 function getScrollPosition() {
@@ -14,8 +15,22 @@ function getScrollPosition() {
     console.log(scrollPosition);
 }
  
+// function getScrollPosition() {
+//     scrollPosition = Math.floor(($(window).scrollTop() / ($(document).height() - $(window).height())) * 100);
+//     scrollRate = scrollPosition;
+//     if(scrollRate >= 25) {
+//         scrollRate - 25
+//     }
+// }
+
+
 // 스크롤을 움직일때마다 해당 함수가 실행
 $(window).scroll(getScrollPosition)
 
 // 배경색 배열(아침 -> 밤-> 아침)
-let bgColor = new Array();
+let bgColor = new Array(
+    "../img/main_gangwon.png", 
+    "../img/main_gyeong.png", 
+    "../img/Jeolla-logo.jpg",
+    "../img/jeju-logo.jpg"
+);
