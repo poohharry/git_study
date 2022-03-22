d14cf3780ce9de92a88f2ef37c62ec940382d559
poohharry <poohharry@naver.com>
2022-03-22 18:04:50 +0900

    스크롤 배경이미지 적용 완료 듀레이션, 배열 인덱스적용 등 아직 손볼게 많음

--- js/main.js
+++ js/main.js
@@ -5,15 +5,20 @@ let scrollRate = 0;
 // 스크롤 비율에 따른 배경색 바꾸기
 function getScrollPosition() {
     scrollPosition = Math.floor(($(window).scrollTop() / ($(document).height() - $(window).height())) * 100);
-    if(scrollPosition < 33 && scrollPosition >= 0) {
-        bg.css("background-color", "lightyellow");
-    }else if(scrollPosition < 66) {
-        bg.css("background-color", "lightskyblue");
+    if(scrollPosition < 25 && scrollPosition >= 0) {
+        // $("#nd01").css({"background":"url(img.png)"});
+        bg.css({"background-image":"url(../img/main_gangwon.png)"});
+    }else if(scrollPosition < 50) {
+        bg.css({"background-image": "url(../img/main_gyeong.png)"});
+    }else if(scrollPosition < 75) {
+        bg.css({"background-image": "url(../img/Jeolla-logo.jpg)"});
     }else if(scrollPosition <= 100) {
-        bg.css("background-color", "lightpink");
+        bg.css({"background-image": "url(../img/jeju-logo.jpg)"});
     }
+
     console.log(scrollPosition);
 }
+
  
 // function getScrollPosition() {
 //     scrollPosition = Math.floor(($(window).scrollTop() / ($(document).height() - $(window).height())) * 100);
