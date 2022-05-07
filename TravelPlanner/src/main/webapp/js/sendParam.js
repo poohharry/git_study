b31b6c47e322ec8d176712e7d0409017c9b44f70
poohharry <poohharry@naver.com>
2022-05-07 13:03:51 +0900

    주석 추가

--- TravelPlanner/src/main/webapp/js/sendParam.js
+++ TravelPlanner/src/main/webapp/js/sendParam.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// onclick에 목적지 url을 담아서 함수를 호출
 function sendParam(pageUrl) {
 	let arr = new Array();
 	/* 장바구니가 비어있을때도 로그인 페이지로 이동가능하게 만들어줄 필요가 있음 */
@@ -16,6 +17,7 @@ function sendParam(pageUrl) {
 				item = arr[i].innerHTML;
 			}
 		}
+		// 장바구니 아이템과 목적지 url을 담아서 get방식으로 넘겨줌
 		let url = "locationProc.jsp"+"?loName=" + item + "&url=" + pageUrl;
 		location.href=url;
 	}
